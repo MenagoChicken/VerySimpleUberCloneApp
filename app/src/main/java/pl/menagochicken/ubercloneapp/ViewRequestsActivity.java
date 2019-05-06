22fee2935f447df4970f4a6d6093502b3cb7d084
MenagoChicken <lewandowski.michau@gmail.com>
2019-05-06 06:01:03 +0200

    display lists of requests - issue

--- app/src/main/java/pl/menagochicken/ubercloneapp/ViewRequestsActivity.java
+++ app/src/main/java/pl/menagochicken/ubercloneapp/ViewRequestsActivity.java
@@ -1,14 +1,140 @@
 package pl.menagochicken.ubercloneapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewRequestsActivity extends AppCompatActivity {
+
+    ListView requestListView;
+    ArrayList<String> requests = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
+    LocationManager locationManager;
+
+    LocationListener locationListener;
+
+    public void updateListView(Location location) {
+
+        if (location != null) {
+
+            requests.clear();
+
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("request");
+
+            final ParseGeoPoint geoPointLocation = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+
+            query.whereNear("location", geoPointLocation);
+            query.setLimit(10);
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if (e != null) {
+                        if (objects.size() > 0) {
+                            for (ParseObject object : objects) {
+                                Double distanceInKm = geoPointLocation.distanceInKilometersTo((ParseGeoPoint) object.get("location"));
+
+                                Double distanceOneDP = (double) Math.round((distanceInKm * 10) / 10);
+
+                                requests.add(distanceOneDP.toString() + " km");
+                            }
+
+                            arrayAdapter.notifyDataSetChanged();
+
+                        } else {
+
+                            requests.add("No active requests nearby");
+
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    Location lastKnownUserLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    updateListView(lastKnownUserLocation);
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_requests);
+
+        requestListView = findViewById(R.id.requestListView);
+
+        requests.clear();
+
+        requests.add("Getting nearby requests...");
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, requests);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                updateListView(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownUserLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (lastKnownUserLocation != null) {
+                updateListView(lastKnownUserLocation);
+            }
+        }
     }
 }
